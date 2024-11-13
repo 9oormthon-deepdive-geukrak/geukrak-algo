c0a67cd5ddf657f0a89b2296bc275d7e2c16298c
kkyung99 <becky0508@naver.com>
2024-11-13 15:47:10 +0900

    feat: 해시를 사용한 풀이 추가

--- 김현경/[LV2] 전화번호 목록.py
+++ 김현경/[LV2] 전화번호 목록.py
@@ -6,3 +6,19 @@ def solution(phone_book):
             return False
     
     return True
+
+# hash 이용한 풀이
+# def solution(phone_book):
+#     phone_dict = {}
+
+#     for nums in phone_book:
+#         phone_dict[nums] = True
+    
+#     for nums in phone_book:
+#         prefix = ""
+#         for num in nums[:-1]: # 자기 자신을 접두어로 체크하지 않기 위해 마지막 문자 제외
+#             prefix += num
+#             if prefix in phone_dict:
+#                 return False
+    
+#     return True
