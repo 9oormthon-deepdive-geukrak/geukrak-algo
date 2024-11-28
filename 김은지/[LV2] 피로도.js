311f0a04c59934e9e6579421cb6255a1cece0694
publdaze <78250089+publdaze@users.noreply.github.com>
2024-11-28 16:27:59 +0900

    [LV2] 피로도 (#22)
    > [문제
    링크](https://school.programmers.co.kr/learn/courses/30/lessons/87946?language=javascript)
    > 시간 소요 `45분/60분`
    > 시간 복잡도 `/O(N!)` N: report의 길이
    
    <img width="200" alt="image"
    src="https://github.com/user-attachments/assets/59980da9-e3f7-4958-91d5-cfd8ddfa0975">
    
    
    ## 풀이
    
    - 일단 순서에 따라서 가능한 던전 수가 달라지므로 모든 순열을 구하기 위해서 permutation 함수를 사용해 모든 순서를
    구해주었습니다.
    - 그 후 스택을 사용해 순차 탐색하며, 필요한 피로도가 충족하지 못하는 경우를 제외하고 탐색하여 총 탐색한 던전 수를 구합니다.
    - 이를 모든 순열을 반복하여, 가장 많은 던전수를 업데이트 해주고 반환합니다.
    
    ## 기타
    - 저번에도 적었던 것 같은데.. 순열이나 조합 구현하는게 매번해도 속도가 안 붙네요.. 언제쯤 속도가 붙으련지..!
    - https://github.com/9oormthon-deepdive-geukrak/geukrak-algo/pull/13 이
    문제에서도 느꼈는데, 순서나 조합 관련된 로직이 필요한 경우에 계산 로직 내부에서 처리하는 게 아니라, 모든 나올 수 있는 그래프
    형태를 만들어 놓고 시작한다거나, 나올 수 있는 순열을 모두 구해놓고 시작하거나 하면서 데이터 형태를 미리 구성해두고 시작하는 걸
    좋아하는 것 같아요. 저는 계산하는 도중에 여러 케이스를 다루는 게 어렵고 코드도 잘 안 읽히는 것 같더라구요..! (쏙함코
    액션보단 계산보단 데이터에서 데이터로 만들어두고 시작하는 느낌?이라서 더 쉽게 느껴지는 것 같기두 ㅎㅎ)

---
+++ 김은지/[LV2] 피로도.js
@@ -0,0 +1,40 @@
+function removeElem(arr, i, cnt) {
+  const copyArr = arr.slice();
+  copyArr.splice(i, cnt);
+  return copyArr;
+}
+
+function permutation(arr) {
+  if (arr.length === 1) return [arr];
+
+  const perm = [];
+  for (let i = 0; i < arr.length; i++) {
+    for (const rest of permutation(removeElem(arr, i, 1))) {
+      perm.push([arr[i], ...rest]);
+    }
+  }
+  return perm;
+}
+
+function explore(remainFatigue, dungeons) {
+  const stack = dungeons;
+
+  let cnt = 0;
+  while (stack.length > 0) {
+    const [needFatigue, payFatigue] = stack.pop();
+
+    if (remainFatigue < needFatigue) continue;
+    cnt++;
+    remainFatigue -= payFatigue;
+  }
+  return cnt;
+}
+
+function solution(k, dungeons) {
+  let maxDungeon = 0;
+  for (const explorationOrder of permutation(dungeons)) {
+    maxDungeon = Math.max(maxDungeon, explore(k, explorationOrder));
+  }
+
+  return maxDungeon;
+}
