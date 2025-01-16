aadb6583e6ccabb84380a07412f487372b699fe5
publdaze <78250089+publdaze@users.noreply.github.com>
2025-01-16 16:46:58 +0900

    [LV2] 점프와 순간 이동 (#53)
    > [문제
    링크](https://school.programmers.co.kr/learn/courses/30/lessons/12980?language=javascript)
    > 시간 소요 `25분/30분`
    > 시간 복잡도 `/O(logN)`
    
    <img width="200" alt="image"
    src="https://github.com/user-attachments/assets/5d163f03-289e-43f8-9aef-8d7ccee02042"
    />
    
    
    ## 풀이
    
    - 처음에 dp를 사용해서 풀었는데, 메모리 초과와 시간 초과가 발생하였습니다.
    - 최종 풀이에서는 *2로 도착되는 곳은 무조건 짝수이기 때문에 도착 지점을 기준으로 짝수일 경우 /2를 하고 홀수일 경우 -1을
    하는 동시에 사용량을 증가시켜주어 최종 사용량을 구하였습니다.

---
+++ 김은지/[LV2] 점프와 순간 이동.js
@@ -0,0 +1,34 @@
+// K칸 앞으로 점프 - K만큼 사용량
+// or
+// 현재까지 온 거리 * 2로 순간이동 - 사용량 X
+// N까지 이동할 때 최솟
+
+function solution(n) {
+  let usage = 0;
+
+  while (n !== 0) {
+    if (n % 2 === 0) {
+      n /= 2;
+    } else {
+      usage++;
+      n -= 1;
+    }
+  }
+
+  return usage;
+}
+
+// 메모리 부족, 시간 초과
+// function solution(n) {
+//     const dp = Array.from({ length: n + 1 }, (_, i) => i);
+
+//     for (let i = 2; i <= n; i++) {
+//         dp[i] = dp[i - 1] + 1;
+
+//         if (i % 2 === 0) {
+//             dp[i] = Math.min(dp[i], dp[i / 2]);
+//         }
+//     }
+
+//     return dp[n];
+// }
