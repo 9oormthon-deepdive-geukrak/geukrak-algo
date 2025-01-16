4c68c138972237062bfde9079d7637a9169f5de9
publdaze <78250089+publdaze@users.noreply.github.com>
2025-01-16 20:03:33 +0900

    [LV0] 캐릭터의 좌표 (#55)

---
+++ 김은지/[LV0] 캐릭터의 좌표.js
@@ -0,0 +1,13 @@
+function solution(keyinput, [width, height]) {
+  const [MAX_X, MAX_Y] = [Math.floor(width / 2), Math.floor(height / 2)];
+  const startPosition = [0, 0];
+
+  const move = {
+    up: ([x, y]) => [x, Math.min(y + 1, MAX_Y)],
+    down: ([x, y]) => [x, Math.max(y - 1, -MAX_Y)],
+    left: ([x, y]) => [Math.max(x - 1, -MAX_X), y],
+    right: ([x, y]) => [Math.min(x + 1, MAX_X), y],
+  };
+
+  return keyinput.reduce((position, key) => move[key](position), startPosition);
+}
