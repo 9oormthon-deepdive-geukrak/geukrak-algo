56722b0a03613d858d5035fe365a67416ba19e4b
publdaze <publdaze@naver.com>
2025-01-25 09:48:03 +0900

    [LV3] 정수 삼각형

---
+++ 김은지/[LV3] 정수 삼각형.js
@@ -0,0 +1,10 @@
+function solution(triangle) {
+  for (let row = triangle.length - 2; row >= 0; row--) {
+    triangle[row] = triangle[row].map(
+      (value, col) =>
+        value +
+        (triangle[row + 1][col] > triangle[row + 1][col + 1] ? triangle[row + 1][col] : triangle[row + 1][col + 1]) // Math.max(triangle[row + 1][col], triangle[row + 1][col + 1])
+    );
+  }
+  return triangle[0][0];
+}
